2cb0d177ee3dd7b0606d501d6d5ae1378fdde734
cfdblaze <coreyfdonaldson@gmail.com>
2015-05-09 15:48:04 -0600

    more tests wh3

--- form_submit.php
+++ form_submit.php
@@ -1,10 +1,9 @@
 <html>
 <body>
-
 Thanks for voting!
 <br>
 You chose <?php echo $_POST["Original"]; ?>, <?php echo $_POST["Withered"]; ?>, <?php echo $_POST["Toys"]; ?>, and <?php echo $_POST["Fright"]; ?>
 <br>
-<? php $file = fopen("fnafvotes.txt", "r", 1); ?>
+<?php $file = fopen("fnafvotes.txt", "r"); ?>
 </body>
 </html>
