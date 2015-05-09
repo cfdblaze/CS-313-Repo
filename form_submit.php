f8c54387326817505313784a1b5c7e1305614090
cfdblaze <coreyfdonaldson@gmail.com>
2015-05-09 16:14:34 -0600

    testing file reassign

--- form_submit.php
+++ form_submit.php
@@ -5,18 +5,17 @@ Thanks for voting!
 You chose <?php echo $_POST["Original"]; ?>, <?php echo $_POST["Withered"]; ?>, <?php echo $_POST["Toys"]; ?>, and <?php echo $_POST["Fright"]; ?>
 <br>
 <?php
-$file = fopen("fnafvotes.txt", "r"); 
+$file = fopen("fnafvotes.txt", "r+"); 
 if($file) {
 $votestring = fgets($file);
-echo $votestring;
 $votesarray = explode(" ", $votestring);
 $votesarray[$_POST["Original"]]++;
 $votesarray[$_POST["Withered"]]++;
 $votesarray[$_POST["Toys"]]++;
 $votesarray[$_POST["Fright"]]++;
-for ($i = 0; $i <= 15; $i++) {
-echo $votesarray[$i] . " ";
-}
+$votestring = implode(" ", $votesarray);
+echo $votestring;
+file_put_contents("fnafvotes.txt", $votestring);
 } else {
 echo "nope";
 }
