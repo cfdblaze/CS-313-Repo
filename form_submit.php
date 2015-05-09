04c8b2c072d9e4dc273ccf5bcf94e46a3e63da4f
cfdblaze <coreyfdonaldson@gmail.com>
2015-05-09 17:18:58 -0600

    back to php

--- form_submit.php
+++ form_submit.php
@@ -42,7 +42,7 @@ if ($votesarray[14] > $votesarray[13])
 $fright = "Phantom Chica";
 if ($votesarray[15] > $votesarray[14])
 $fright = "Phantom Foxy";
-echo $original . "\n" . $withered . "\n" . $toy . "\n" . $fright;
+echo $original . " " . $withered . " " . $toy . " " . $fright;
 } else {
 echo "nope";
 }
