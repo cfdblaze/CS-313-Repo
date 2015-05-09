5e71b29ec76003739d019b89e35b2a76f55e7482
cfdblaze <coreyfdonaldson@gmail.com>
2015-05-09 14:48:23 -0600

    haaagh

--- form_submit.php
+++ form_submit.php
@@ -7,14 +7,9 @@ You chose <?php echo $_POST["Original"]; ?>, <?php echo $_POST["Withered"]; ?>, 
 <br>
 <? php
 $file = fopen("fnafvotes.txt", "r");
-echo "HI";
 if($file) {
 $votestring = fgets($file);
 $votesarray = explode(" ", $votestring);
-$votesarry[$_POST["Original"]]++;
-$votesarry[$_POST["Withered"]]++;
-$votesarry[$_POST["Toys"]]++;
-$votesarry[$_POST["Fright"]]++;
 for ($i = 0; $i <= 15; $i++) {
 echo $votesarry[$i] . " ";
 }
