985afd394c092b1b016b586e80bb1e7a5cb579f2
cfdblaze <coreyfdonaldson@gmail.com>
2015-05-09 13:11:53 -0600

    first submit test

---
+++ form_submit.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Thanks for voting!
+<br>
+You chose <?php echo $_POST["Original"]; ?>, <?php echo $_POST["Withered"]; ?>, <?php echo $_POST["Toys"]; ?>, and <?php echo $_POST["Fright"]; ?>
+
+</body>
+</html>
