519472c799204e020c0d9007f3357e00b6561ff2
cfdblaze <coreyfdonaldson@gmail.com>
2015-05-09 15:09:11 -0600

    more tests whaaa

--- form_submit.php
+++ form_submit.php
@@ -5,17 +5,5 @@ Thanks for voting!
 <br>
 You chose <?php echo $_POST["Original"]; ?>, <?php echo $_POST["Withered"]; ?>, <?php echo $_POST["Toys"]; ?>, and <?php echo $_POST["Fright"]; ?>
 <br>
-<? php
-$file = fopen("fnafvotes.txt", "r");
-if($file) {
-$votestring = fgets($file);
-$votesarray = explode(" ", $votestring);
-for ($i = 0; $i <= 15; $i++) {
-echo $votesarry[$i] . " ";
-}
-} else {
-echo "nope";
-}
-?>
 </body>
 </html>
