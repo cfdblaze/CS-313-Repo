4618439a31c77b54a65d269af76563de34fb9fe6
cfdblaze <coreyfdonaldson@gmail.com>
2015-05-09 15:50:26 -0600

    that was the problem AAAAAGH

--- form_submit.php
+++ form_submit.php
@@ -4,6 +4,21 @@ Thanks for voting!
 <br>
 You chose <?php echo $_POST["Original"]; ?>, <?php echo $_POST["Withered"]; ?>, <?php echo $_POST["Toys"]; ?>, and <?php echo $_POST["Fright"]; ?>
 <br>
-<?php $file = fopen("fnafvotes.txt", "r"); ?>
+<?php
+$file = fopen("fnafvotes.txt", "r"); 
+if($file) {
+$votestring = fgets($file);
+$votesarray = explode(" ", $votestring);
+$votesarry[$_POST["Original"]]++;
+$votesarry[$_POST["Withered"]]++;
+$votesarry[$_POST["Toys"]]++;
+$votesarry[$_POST["Fright"]]++;
+for ($i = 0; $i <= 15; $i++) {
+echo $votesarry[$i] . " ";
+}
+} else {
+echo "nope";
+}
+?>
 </body>
 </html>
